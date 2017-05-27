c57ed033738bf82e99d256fc2bdb3e6632d1b3ff
Rogerio Carignano Nascimento <rogerio.carignano@gmail.com>
2017-05-27 20:57:08 -0300

    Update MapTemperaturaMedia.java

--- Temperatura/MapReduce/src/MapTemperaturaMedia.java
+++ Temperatura/MapReduce/src/MapTemperaturaMedia.java
@@ -1,33 +1,22 @@
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
-
 import java.io.IOException;
 
-public class MapTemperaturaMedia extends Mapper <LongWritable, Text, IntWritable, FloatWritable>
-{
+public class MapTemperaturaMedia extends Mapper <LongWritable, Text, IntWritable, FloatWritable> {
            
-
-           
-		public void map(LongWritable key, Text value, Context context) throws                                                                  IOException,  InterruptedException
-            {
+		public void map(LongWritable key, Text value, Context context) throws IOException,  InterruptedException {
 						
-						
-	   					float temperatura;
-	   					
+	   		float temperatura;
                         String[] linha = value.toString().split(";"); //coloca cada campo em uma posicao no vetor linha                       
-                                                                                                                     
                         String[] aux_ano = linha[1].toString().split("/"); //
-                        
                         int mes = Integer.parseInt(aux_ano[1].toString()); 
                         
-                                                
                         if (linha[4].length() > 0 && linha[4] != null)
-                                    temperatura = Float.parseFloat(linha[4]);
+                           	temperatura = Float.parseFloat(linha[4]);
                         else
-                        			temperatura = Float.parseFloat(linha[5]);      
+                        	temperatura = Float.parseFloat(linha[5]);      
                        
-                          
-                       	context.write(new IntWritable(mes),new FloatWritable(temperatura));
+                        context.write(new IntWritable(mes),new FloatWritable(temperatura));
  
             }
 }
