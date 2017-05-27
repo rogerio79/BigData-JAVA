dc9b183e1377a5c7335a6608d23c4afe7c1b0436
Rogerio Carignano Nascimento <rogerio.carignano@gmail.com>
2017-05-27 20:58:31 -0300

    Update ReduceTemperaturaMedia.java

--- Temperatura/MapReduce/src/ReduceTemperaturaMedia.java
+++ Temperatura/MapReduce/src/ReduceTemperaturaMedia.java
@@ -1,28 +1,22 @@
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.*;
-
 import java.io.IOException;
 
-public class ReduceTemperaturaMedia extends Reducer <IntWritable, FloatWritable,IntWritable, FloatWritable > {
-	public void reduce(IntWritable key,  Iterable<FloatWritable> values, Context context) throws IOException,                                                       InterruptedException
-            {          
-			Float temperaturas = new Float(0);
-			
-			int contador = 0;
-            
+public class ReduceTemperaturaMedia extends Reducer <IntWritable, FloatWritable,IntWritable, FloatWritable> {
+	public void reduce(IntWritable key,  Iterable<FloatWritable> values, Context context) throws IOException,InterruptedException {          
+	    Float temperaturas = new Float(0);
+	    int contador = 0;
             
             for (FloatWritable value : values) {
             	temperaturas += value.get();
             	contador += 1;            	
-            }
-            try {
+            } try {
             	temperaturas = temperaturas / contador;
             } finally {
             	contador = 1;
             }
             
             context.write(key, new FloatWritable(temperaturas));
-           
     }                                             
 }
